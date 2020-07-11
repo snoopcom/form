ba4eb8fc7ac1d9d5a93742c62dc82e7ed7f76ccd
Alex <snoopcom@bk.ru>
2020-07-11 10:00:55 +0300

    Updates

---
+++ precache-manifest.38e95bc4f058f9d093443f51d1cd1417.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de3a0b82e702cce235ccdc9e587880a",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "45d6194c568fb660f188",
+    "url": "/form/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "26b9ba26c82da442bc26",
+    "url": "/form/static/js/2.93798b1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/form/static/js/2.93798b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d6194c568fb660f188",
+    "url": "/form/static/js/main.0243783c.chunk.js"
+  },
+  {
+    "revision": "88bc9024cd2a3308059e",
+    "url": "/form/static/js/runtime-main.fd21892c.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/form/static/media/logo.ee7cd8ed.svg"
+  }
+]);
